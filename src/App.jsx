e2c997888ccdb0e12fb47afc002eb3857247aac1
sailesh <vermasailesh271@gmail.com>
2023-09-24 17:37:44 +0530

    added basic ui and todo fucntionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,79 @@
+import "./style.css";
+import { useState } from "react";
+
 function App() {
-  return "Hii";
+  const [item, setItem] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+
+    addTodo({ id: crypto.randomUUID(), title: item, isChecked: false });
+  }
+
+  function addTodo(toDoItem) {
+    setTodoList((currentToDos) => {
+      return [...currentToDos, { ...toDoItem }];
+    });
+
+    setItem("");
+  }
+
+  function deleteToDoItem(id) {
+    setTodoList((currentToDos) => {
+      return currentToDos.filter((item) => item.id != id);
+    });
+  }
+
+  function toggleToDo(id, isChecked) {
+    setTodoList((currentToDos) => {
+      return currentToDos.map((item) => {
+        if (item.id == id) {
+          return { ...item, isChecked };
+        }
+      });
+    });
+  }
+
+  return (
+    <>
+      <form className="new-item-form" onSubmit={onFormSubmit}>
+        <label>Enter your work here</label>
+        <input
+          type="text"
+          value={item}
+          onChange={(val) => setItem(val.target.value)}
+        />
+
+        <button className="btn">Add Work</button>
+      </form>
+      <h1>Your Today Work List</h1>
+      <ul className="list">
+        {todoList.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.isChecked}
+                  onChange={(e) => {
+                    toggleToDo(todo.id, e.target.checked);
+                  }}
+                />
+                {todo.title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteToDoItem(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
